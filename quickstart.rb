18464eb1bcc47174d2c7f7146de80188d2b12b34
jordan <jordantmunoz@gmail.com>
2017-07-16 23:02:00 -0700

    updates quickstart

--- quickstart.rb
+++ quickstart.rb
@@ -3,3 +3,53 @@ require 'googleauth'
 require 'googleauth/stores/file_token_store'
 
 require 'fileutils'
+
+
+OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
+APPLICATION_NAME = 'Google Slides API Ruby Quickstart'
+CLIENT_SECRETS_PATH = 'client_secret.json'
+CREDENTIALS_PATH = File.join(Dir.home, '.credentials',
+                             "slides.googleapis.com-ruby-quickstart.yaml")
+SCOPE = Google::Apis::SlidesV1::AUTH_PRESENTATIONS_READONLY
+
+##
+# Ensure valid credentials, either by restoring from the saved credentials
+# files or intitiating an OAuth2 authorization. If authorization is required,
+# the user's default browser will be launched to approve the request.
+#
+# @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
+def authorize
+  FileUtils.mkdir_p(File.dirname(CREDENTIALS_PATH))
+
+  client_id = Google::Auth::ClientId.from_file(CLIENT_SECRETS_PATH)
+  token_store = Google::Auth::Stores::FileTokenStore.new(file: CREDENTIALS_PATH)
+  authorizer = Google::Auth::UserAuthorizer.new(
+    client_id, SCOPE, token_store)
+  user_id = 'default'
+  credentials = authorizer.get_credentials(user_id)
+  if credentials.nil?
+    url = authorizer.get_authorization_url(
+      base_url: OOB_URI)
+    puts "Open the following URL in the browser and enter the " +
+         "resulting code after authorization"
+    puts url
+    code = gets
+    credentials = authorizer.get_and_store_credentials_from_code(
+      user_id: user_id, code: code, base_url: OOB_URI)
+  end
+  credentials
+end
+
+# Initialize the API
+service = Google::Apis::SlidesV1::SlidesService.new
+service.client_options.application_name = APPLICATION_NAME
+service.authorization = authorize
+# Prints the number of slides and elements in a sample presentation:
+# https://docs.google.com/presentation/d/1EAYk18WDjIG-zp_0vLm3CsfQh_i8eXc67Jo2O9C6Vuc/edit
+presentation_id = '1EAYk18WDjIG-zp_0vLm3CsfQh_i8eXc67Jo2O9C6Vuc'
+presentation = service.get_presentation(presentation_id)
+
+puts "The presentation contains #{presentation.slides.count} slides:"
+presentation.slides.each_with_index do |slide, i|
+  puts "- Slide \##{i + 1} contains #{slide.page_elements.count} elements."
+end
